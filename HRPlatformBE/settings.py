84ab3191d95c98947033d990db6db2b3b59742ac
hemantpaliwal <hemant.paliwal@icicibank.com>
2024-01-16 15:16:35 +0530

    added models

--- HRPlatformBE/settings.py
+++ HRPlatformBE/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-tpcfr)kd7hhf6buuh*wen+#9z3@i6@o%mt(jx-%$79sc=c+=em"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
@@ -37,6 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user",
+    "test_app",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = "HRPlatformBE.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hr_platform',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
+import uuid
+
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+# Department, Employee, Candidate
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class Department(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    head = models.ForeignKey(Employee, null=True, blank=True, on_delete=models.SET_NULL)
+    requirements = ArrayField(models.CharField(max_length=100),size=50)
+
+    def __str__(self):
+        return self.name
