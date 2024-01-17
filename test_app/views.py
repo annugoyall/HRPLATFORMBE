95025b301fc67a3a469f5ea2c0cd33dfa1a8533d
Annu Goyal <annugoyal@Annus-MacBook-Air.local>
2024-01-17 13:42:33 +0530

    Questions viewset

--- test_app/views.py
+++ test_app/views.py
@@ -1,15 +1,63 @@
-# views.py
-
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Test
-from .serializers import TestSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 
-class TestCreateView(APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = TestSerializer(data=request.data)
+from test_app.models import Test, Question, TestResponse
+from test_app.serializers import TestSerializer, QuestionSerializer, TestResponseSerializer
+
+
+class TestViewSet(ModelViewSet):
+    serializer_class = TestSerializer
+    queryset = Test.objects.all()
+
+    def patch(self, request, *args, **kwargs):
+        test_id = kwargs.get('pk')
+        test = Test.objects.get(pk=test_id)
+        serializer = TestSerializer(test, data=request.data, partial=True)
+
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class QuestionViewSet(ModelViewSet):
+    serializer_class = QuestionSerializer
+    queryset = Question.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = QuestionSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, *args, **kwargs):
+        try:
+            question_id = request.data.get("id")
+            question = Question.objects.get(pk=question_id)
+            serializer = QuestionSerializer(question, data=request.data, partial=True)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
+
+class TestResponseViewSet(ModelViewSet):
+    serializer_class = TestResponseSerializer
+    queryset = TestResponse.objects.all()
+
+    def patch(self, request, *args, **kwargs):
+        test_id = kwargs.get('pk')
+        test = TestResponse.objects.get(pk=test_id)
+        serializer = TestSerializer(test, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
