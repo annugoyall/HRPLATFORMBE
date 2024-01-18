d101446771510416f153fd3660f38b3c059c2c72
Annu Goyal <annugoyal@Annus-MacBook-Air.local>
2024-01-18 20:28:36 +0530

    Changes in test response

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TestViewSet, QuestionViewSet, TestResponseViewSet
+from .views import TestViewSet, QuestionViewSet, TestResponseViewSet, GetCandidateTestResponseView
 
 router = DefaultRouter()
 router.register(r'test', TestViewSet, basename='TestView')
@@ -9,5 +9,6 @@ router.register(r'test_response', TestResponseViewSet, basename='TestView')
 
 
 urlpatterns = [
+    path('get_candidate_test_response/<id>', GetCandidateTestResponseView.as_view(), name='get_candidate_test_response'),
     path('', include(router.urls)),
 ]
