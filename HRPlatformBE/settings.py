171a124ec2ed94e6992c7e6f468a9b0c2969815a
hemantpaliwal <hemant.paliwal@icicibank.com>
2024-01-18 14:41:03 +0530

    cors error

--- HRPlatformBE/settings.py
+++ HRPlatformBE/settings.py
@@ -34,7 +34,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000','http://13.233.194.145'
 ]
 # Application definition
-CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOW_ALL_ORIGINS = True
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -56,7 +57,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "HRPlatformBE.urls"
